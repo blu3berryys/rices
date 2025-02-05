41e06d675bb88602accb1e0a30a283c975aa4dd5
blu3 <blu3berry@disroot.org>
2025-02-05 14:36:47 +0300

    Update ddl_1.0.0.sql
    Signed-off-by: blu3 <blu3berry@disroot.org>

--- sql/ddl_1.0.0.sql
+++ sql/ddl_1.0.0.sql
@@ -6,14 +6,14 @@ CREATE TABLE rices (
     version VARCHAR(10) NOT NULL,          -- Data version
     os VARCHAR(30) NOT NULL,               -- Operating system
     name VARCHAR(75) NOT NULL,             -- Name of the rice
-    author VARCHAR(100) NOT NULL,             -- Name of the rice
+    author VARCHAR(100) NOT NULL,          -- Name of the rice
     token UUID NOT NULL,                   -- Unique authorization token
     visits INTEGER DEFAULT 0 NOT NULL,     -- Visit counter, initialized to 0
     level INTEGER DEFAULT 0 NOT NULL,      -- Level: 0 (Public), 1 (Verified)
     created_at TIMESTAMP DEFAULT NOW(),    -- Creation date
     updated_at TIMESTAMP,                  -- Last update date
-    PRIMARY KEY (id),                -- Composite primary key
-    UNIQUE (slug)                         -- Ensure slug is unique
+    PRIMARY KEY (id),                      -- Composite primary key
+    UNIQUE (slug)                          -- Ensure slug is unique
 );
 
 CREATE OR REPLACE FUNCTION increment_visits(slug_param TEXT)
